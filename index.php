a0691c2f02ef19f50a43718c360758a632817d73
minhho0598 <37680096+minhho0598@users.noreply.github.com>
2019-12-02 12:40:22 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+if (isset($_POST['idProvince'])) {
+    $idProvince = $_POST['idProvince'];
+    $url = "https://thongtindoanhnghiep.co/api/city/" . $idProvince . "/district";
+    $result = sendRequest($url);
+    if ($result == false) {
+        echo json_encode(array("status" => false));
+    }
+    else {
+        echo $result;
+    }
+}
+
+if (isset($_POST['idDistrict'])) {
+    $idDistrict = $_POST['idDistrict'];
+    $url = "https://thongtindoanhnghiep.co/api/district/" . $idDistrict . "/ward";
+    $result = sendRequest($url);
+    if ($result == false) {
+        echo json_encode(array("status" => false));
+    }
+    else {
+        echo $result;
+    }
+}
+
+function sendRequest($url, $data = array()){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $output = curl_exec($ch);
+    curl_close($ch);
+    
+    return $output;
+}
