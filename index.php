76a17a5347b636df456b73dbda65b2528fa8b967
minhho0598 <37680096+minhho0598@users.noreply.github.com>
2019-12-30 08:02:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$uri = $_SERVER["REQUEST_URI"];
-$url = "https://thongtindoanhnghiep.co/api/" . $uri;
-echo $url;
-// $result = sendRequest($url);
-// if ($result == false) {
-//     echo json_encode(array("status" => false));
-// }
-// else {
-//     echo $result;
-// }
-// if (isset($_POST['idProvince'])) {
-//     $idProvince = $_POST['idProvince'];
-//     $url = ""https://thongtindoanhnghiep.co/api/city/" . $idProvince . "/district";
-//     $result = sendRequest($url);
-//     if ($result == false) {
-//         echo json_encode(array("status" => false));
-//     }
-//     else {
-//         echo $result;
-//     }
-// }
+if (isset($_POST['idProvince'])) {
+    $idProvince = $_POST['idProvince'];
+    $url = ""https://thongtindoanhnghiep.co/api/city/" . $idProvince . "/district";
+    $result = sendRequest($url);
+    if ($result == false) {
+        echo json_encode(array("status" => false));
+    }
+    else {
+        echo $result;
+    }
+}
 
-// if (isset($_POST['idDistrict'])) {
-//     $idDistrict = $_POST['idDistrict'];
-//     $url = "https://thongtindoanhnghiep.co/api/district/" . $idDistrict . "/ward";
-//     $result = sendRequest($url);
-//     if ($result == false) {
-//         echo json_encode(array("status" => false));
-//     }
-//     else {
-//         echo $result;
-//     }
-// }
+if (isset($_POST['idDistrict'])) {
+    $idDistrict = $_POST['idDistrict'];
+    $url = "https://thongtindoanhnghiep.co/api/district/" . $idDistrict . "/ward";
+    $result = sendRequest($url);
+    if ($result == false) {
+        echo json_encode(array("status" => false));
+    }
+    else {
+        echo $result;
+    }
+}
 
 function sendRequest($url, $data = array()){
     $ch = curl_init();
