d60bedca1d1eb9807b12747f757b1caa4788c1ff
minhho0598 <37680096+minhho0598@users.noreply.github.com>
2019-12-30 08:00:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@ header("Access-Control-Allow-Origin: *");
 
 $uri = $_SERVER["REQUEST_URI"];
 $url = "https://thongtindoanhnghiep.co/api/" . $uri;
-$result = sendRequest($url);
-if ($result == false) {
-    echo json_encode(array("status" => false));
-}
-else {
-    echo $result;
-}
+echo $url;
+// $result = sendRequest($url);
+// if ($result == false) {
+//     echo json_encode(array("status" => false));
+// }
+// else {
+//     echo $result;
+// }
 // if (isset($_POST['idProvince'])) {
 //     $idProvince = $_POST['idProvince'];
 //     $url = ""https://thongtindoanhnghiep.co/api/city/" . $idProvince . "/district";
